20d8169da37f6ea9445ce5eb5c597cb9ac036884
Bhagyashri Sadawarte <sadawarte.bhagyashri@gmail.com>
2022-08-07 18:47:24 +0530

    added one line of code

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+CSV_FILE_PATH = os.path.join('project_app' , 'house_dataset.csv')
+MODEL_FILE_PATH = os.path.join('project_app' , 'linear_reg_model.pkl')
+JSON_FILE_PATH = os.path.join('project_app' , 'project_data.json')
+
+
+#PORT_NUMBER = 5005
